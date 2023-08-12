c1433f46aefc8424c4ca1dd9fcacf3ed5cf711c9
Dev-kami <blossomobasi2@gmail.com>
2023-08-12 20:21:28 +0100

    committed

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  const main = document.getElementsByTagName("main")[0];
+  const container = document.querySelector(".container");
+  main.classList.add("show");
+  container.classList.add("show");
+});
